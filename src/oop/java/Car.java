937f00f984f05e1a858bc6242dbd5ca0472a0301
unknown <ahmed.aga2001@gmail.com>
2021-03-30 15:43:24 +0200

    Secound edit

--- src/oop/java/Car.java
+++ src/oop/java/Car.java
@@ -1,24 +1,25 @@
-
 package oop.java;
 
 public class Car {
-  private float speed;
-  private float distance;
-  private float time;
-  
-  public float gettime(){
-  
-      this.time =  distance/speed  ;
-      return time;
-  }
-  
-  public void setspeed(float s){
 
-  speed=s;
-}
-  public void setdistance (float d){
-  
-      distance = d;
-  }
-  
+    private float speed;
+    private float distance;
+    private float time;
+
+    public float gettime() {
+
+        this.time = distance / speed;
+        return time;
+    }
+
+    public void setspeed(float speed) {
+
+        this.speed = speed;
+    }
+
+    public void setdistance(float distance) {
+
+        this.distance = distance;
+    }
+
 }
